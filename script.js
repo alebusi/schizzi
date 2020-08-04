0585ca926b2b048cd2e70e5b2a5e5ff1be5b5e82
alebusi <allebusi@gmail.com>
2020-08-04 12:13:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,12 @@ function avanti() {
        if (i < testo.length-1) {
          i+=1;
          r_t = testo[i];
-         current_string = current_string.concat(r_t);
+	 if (r_t == chr(32)) {
+	    current_string = current_string.concat('<span style="color:black;">-</span>'); 
+	 }
+	 else {
+            current_string = current_string.concat(r_t);
+	 }
          document.getElementById("testo").innerHTML = current_string;
 	 nc+=1;
          if (nc > nc_max-1) {
