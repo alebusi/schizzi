e23bc92f20ef6a23973c0be1a1c299604729d144
alebusi <allebusi@gmail.com>
2021-06-01 12:43:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-    $("#testoCompleto").load("https://docs.google.com/document/d/e/2PACX-1vRDihSpthOhnhcIbIi7Z1OfArt2KtozYcXuglANaXJk-xDDWgN9AXM6go_HFLoCaufZjWZV5EEKpq_P/pub span");
+    $("#testoCompleto").load("https://docs.google.com/document/d/e/2PACX-1vR2wy7N8nsgFMNHXOhIozb2PXqPEyEx0Xff6BXvgE6L2ywGaE0_NDZ46fjef7Z8sqSpMGvPgKQH1CZT/pub span");
 });
