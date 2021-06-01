16fe6c9203fbcff03669224382a416e9efebc429
alebusi <allebusi@gmail.com>
2021-06-01 17:57:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $("#testoCompleto").load("https://docs.google.com/document/d/e/2PACX-1vR2wy7N8nsgFMNHXOhIozb2PXqPEyEx0Xff6BXvgE6L2ywGaE0_NDZ46fjef7Z8sqSpMGvPgKQH1CZT/pub span");
 });
+
+function elaboraTesto() {
+  testo=document.getElementById("testoCompleto").innerHTML;
+  testo=testo.replace(/&lt;/g,"<");
+  testo=testo.replace(/&gt;/g,">");
+  document.getElementById("logo").innerHTML = testo;
+}
