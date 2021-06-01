0e74b1d5ffce9ccd447b9ebc41e78bf1e1234d03
alebusi <allebusi@gmail.com>
2021-06-01 20:44:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,6 @@ function elaboraTesto() {
   testo=testo.replace(/&gt;/g,">");
   document.getElementById("logo").innerHTML = testo;
 }
-$(document).ready(function(){
-    $("#testoCompleto").load("https://docs.google.com/document/d/e/2PACX-1vR2wy7N8nsgFMNHXOhIozb2PXqPEyEx0Xff6BXvgE6L2ywGaE0_NDZ46fjef7Z8sqSpMGvPgKQH1CZT/pub span");
-});
-
-function elaboraTesto() {
-  testo=document.getElementById("testoCompleto").innerHTML;
-  testo=testo.replace(/&lt;/g,"<");
-  testo=testo.replace(/&gt;/g,">");
-  document.getElementById("logo").innerHTML = testo;
-}
 
 function myFunc() {
   a=1; //funzione fittizia per far scattare hover su disp.touch
